2e4b2659ec5d7249482b06c7c20e7b765d365806
NowakArtur97 <nowart97@gmail.com>
2023-05-29 17:35:25 +0200

    Generate project

---
+++ publisher/src/test/java/com/nowakartur97/yourdelivery/publisher/PublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowakartur97.yourdelivery.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
