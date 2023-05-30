6db929e62de60669cdcef11afd5f2199879b4889
NowakArtur97 <nowart97@gmail.com>
2023-05-30 12:58:03 +0200

    Generate consumer project

---
+++ consumer/src/test/java/com/nowakartur97/yourdelivery/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowakartur97.yourdelivery.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
